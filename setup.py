7aff5cbdcb930d1f23a1cac0a8352a68ea1cd718
Vo Van Nghia <vanvnghia@gmail.com>
2022-06-17 20:58:23 +0200

    demo: cleanup build file and make fastor faster

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
+from setuptools import find_packages
 from skbuild import setup
-
 
 setup(
     name="planning",
     version="0.0.1",
-    packages=["planning"],
+    packages=find_packages(where="planning", exclude=["planning.*"]),
     package_dir={"": "planning"},
     cmake_install_dir="planning/planning",
     include_package_data=True,
