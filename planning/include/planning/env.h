acb14b7571d61796e0813511a2d29d128a32c6d7
Vo Van Nghia <vanvnghia@gmail.com>
2022-06-22 02:22:32 +0200

    demo: refactor env for faster performance

--- planning/include/planning/env.h
+++ planning/include/planning/env.h
@@ -22,9 +22,8 @@ static constexpr T gen_iota(std::integer_sequence<int_type, i...>) {
   return {i...};
 }
 
-template <int_type n_transition, int_type n_queue, int_type n_env,
-          int_type... i>
-static constexpr auto gen_trans(std::integer_sequence<int_type, i...>) {
+template <int_type n_transition, int_type n_queue, int_type... i>
+static constexpr auto gen_trans(std::integer_sequence<int_type, i...> is) {
   std::array<int_type, n_transition> trans{};
   std::fill_n(trans.begin(), n_queue, 1);
   std::fill_n(trans.begin() + n_queue, n_queue, -1);
@@ -32,11 +31,35 @@ static constexpr auto gen_trans(std::integer_sequence<int_type, i...>) {
   return trans;
 }
 
-template <typename T, typename... prefixes_type, int_type... i,
-          typename... postfixes_type>
+template <const auto& a, int_type i, int_type... j, int_type... k>
+static constexpr auto gen_inf_index(std::integer_sequence<int_type, j...>,
+                                    std::integer_sequence<int_type, k...>) {
+  return std::make_tuple(Fastor::fseq<0, a[j]>{}..., Fastor::fseq<0, 1>{},
+                         Fastor::fseq<0, a[k + i + 1]>{}..., Fastor::fix<i>);
+}
+
+template <const auto& a, int_type... i>
+static constexpr auto gen_inf_indices(std::integer_sequence<int_type, i...> is) {
+  return std::make_tuple(
+      gen_inf_index<a, i>(std::make_integer_sequence<int_type, i>{},
+                          std::make_integer_sequence<int_type, is.size() - i - 1>{})...);
+}
+
+template <int_type offset, int_type idx, int_type... i>
+static constexpr auto state_prob_assign(auto& p, const auto& e, const auto& s, const auto& a,
+                                        std::integer_sequence<int_type, i...>) {
+  ((p(offset + i) = a(i) * e(s(i), i, idx)), ...);
+}
+
+template <int_type offset, int_type idx, int_type... i>
+static constexpr auto env_prob_assign(auto& p, const auto& e, const auto& a,
+                                      std::integer_sequence<int_type, i...>) {
+  ((p(offset + i) = a(i) * e(idx, i)), ...);
+}
+
+template <typename T, typename... prefixes_type, int_type... i, typename... postfixes_type>
 static constexpr auto to_index(const T& a, prefixes_type... prefixes,
-                               std::integer_sequence<int_type, i...>,
-                               postfixes_type... postfixes) {
+                               std::integer_sequence<int_type, i...>, postfixes_type... postfixes) {
   if constexpr (is_indexable<T>) {
     return std::make_tuple(prefixes..., a[i]..., postfixes...);
   } else {
@@ -44,25 +67,30 @@ static constexpr auto to_index(const T& a, prefixes_type... prefixes,
   }
 }
 
-template <int_type n_env_t, int_type n_queue_t, typename F, bool save_qs_t,
-          int_type... max_lens>
+template <int_type n_env_t, int_type n_queue_t, typename F, bool save_qs_t, int_type... max_lens_t>
 class Env {
  public:
+  static constexpr auto n_env = n_env_t;
+  static constexpr auto n_queue = n_queue_t;
+
   using float_type = F;
 
-  static constexpr auto n_queue = n_queue_t;
-  static constexpr auto n_env = n_env_t;
   static constexpr auto save_qs = save_qs_t;
+
+  static constexpr auto dim_queue = std::array{max_lens_t + 1 ...};
+
   static constexpr auto n_transition = 2 * n_queue + n_queue * n_env;
-  static constexpr auto n_total = ((max_lens + 1) * ...) * n_queue;
 
   using env_param_type = Fastor::Tensor<float_type, n_env, n_queue, 3>;
+  using env_prob_type = Fastor::Tensor<float_type, n_env, n_queue>;
+
   using prob_type = Fastor::Tensor<float_type, n_transition + 1>;
 
-  using q_type = Fastor::Tensor<float_type, max_lens + 1 ..., n_queue>;
-  using n_visit_type = Fastor::Tensor<int_type, max_lens + 1 ..., n_queue>;
+  using q_type = Fastor::Tensor<float_type, max_lens_t + 1 ..., n_queue>;
+  using n_visit_type = Fastor::Tensor<int_type, max_lens_t + 1 ..., n_queue>;
+
   using qs_type = std::vector<float_type>;
-  using env_prob_type = Fastor::Tensor<float_type, n_env, n_queue>;
+  static constexpr auto n_total = q_type::size();
 
   using array_fq_type = Fastor::Tensor<float_type, n_queue>;
   using array_iq_type = Fastor::Tensor<int_type, n_queue>;
@@ -74,12 +102,11 @@ class Env {
 
   using iq_type = Fastor::TensorMap<int_type, n_queue>;
 
-  static constexpr std::array<int_type, n_queue> dim_queue = {max_lens + 1 ...};
-  static constexpr auto idx_nq =
-      std::make_integer_sequence<int_type, n_queue>{};
+  static constexpr auto idx_nq = std::make_integer_sequence<int_type, n_queue>{};
   static constexpr auto idx_ne = std::make_integer_sequence<int_type, n_env>{};
-  static constexpr auto iota_nq =
-      gen_iota<std::array<int_type, n_queue>>(idx_nq);
+  static constexpr auto iota_nq = gen_iota<std::array<int_type, n_queue>>(idx_nq);
+
+  static constexpr auto inf_indices = gen_inf_indices<dim_queue>(idx_nq);
 
   Env(const env_param_type& env_param, const env_prob_type& env_prob)
       : env_param_(env_param), env_prob_(env_prob) {}
@@ -89,65 +116,44 @@ class Env {
 
     std::apply(
         [this](auto&&... idx) {
-          ((std::apply(this->q_, idx) =
-                -std::numeric_limits<float_type>::infinity()),
-           ...);
+          ((std::apply(this->q_, idx) = -std::numeric_limits<float_type>::infinity()), ...);
         },
-        gen_inf_indices(idx_nq));
-    std::apply(
-        q_, to_index(std::array<decltype(Fastor::fix<0>), n_queue + 1>{},
-                     std::make_integer_sequence<int_type, n_queue + 1>{})) = 0;
+        inf_indices);
+    std::apply(q_, to_index(std::array<int_type, n_queue + 1>{},
+                            std::make_integer_sequence<int_type, n_queue + 1>{})) = 0;
 
     n_visit_.zeros();
   }
 
   void reset(std::mt19937_64::result_type seed = 42) {
-    rng.seed(seed);
+    rng_.seed(seed);
     states_.zeros();
     env_states_.zeros();
   }
 
   prob_type prob(int_type action) {
     array_bq_type allow_u = (states_ < max_lens_);
-    array_fq_type pus;
-    for (size_t i = 0; i < n_queue; ++i) {
-      pus(i) = env_param_(env_states_(i), i, 1);
-    }
-
-    pus *= allow_u.template cast<float_type>();
-
     array_bq_type allow_d = (action == actions_) && (states_ > 0);
-    array_fq_type pds;
-    for (size_t i = 0; i < n_queue; ++i) {
-      pds(i) = env_param_(env_states_(i), i, 2);
-    }
-    pds *= allow_d.template cast<float_type>();
 
     prob_type p;
-    p(Fastor::fseq<0, n_queue>{}) = pus;
-    p(Fastor::fseq<n_queue, 2 * n_queue>{}) = pds;
-    float_type prob_dummy = 1 - sum(pus) - sum(pds);
+    state_prob_assign<0, 1>(p, env_param_, env_states_, allow_u, idx_nq);
+    state_prob_assign<n_queue, 2>(p, env_param_, env_states_, allow_d, idx_nq);
 
-    array_be_type allow_i;
-    array_fq_type pis;
-    for (int_type i = 0; i < n_env; ++i) {
-      allow_i = (env_states_ != i);
-      pis = env_prob_(i, Fastor::all);
-      pis *= allow_i.template cast<float_type>();
-      prob_dummy -= Fastor::sum(pis);
-      p(Fastor::seq(2 * n_queue + i * n_queue,
-                    2 * n_queue + (i + 1) * n_queue)) = pis;
-    }
+    array_bq_type allow_e;
+    ([ this, &allow_e, &p ]<int_type... i>(std::integer_sequence<int_type, i...>) {
+      ((allow_e = (this->env_states_ != i),
+        env_prob_assign<2 * n_queue + i * n_queue, i>(p, env_prob_, allow_e, idx_nq)),
+       ...);
+    })(idx_ne);
 
-    p(Fastor::fix<-1>) = prob_dummy;
+    p(-1) = 1 - sum(p(Fastor::fseq<0, n_transition>{}));
 
     return p;
   }
 
   array_iq_type step(int_type action) {
     auto p = prob(action);
-    auto idx = std::discrete_distribution<int_type>(
-        p.data(), p.data() + n_transition + 1)(rng);
+    auto idx = std::discrete_distribution<int_type>(p.data(), p.data() + n_transition + 1)(rng_);
     array_iq_type nstates = states_;
 
     const auto idx_q = idx % n_queue;
@@ -159,14 +165,13 @@ class Env {
     return nstates;
   }
 
-  virtual float_type reward(const array_iq_type& states,
-                            const array_iq_type& env_states) const {
+  virtual float_type reward(const array_iq_type& states, const array_iq_type& env_states) const {
     return 0;
   }
 
-  void train(float_type gamma = 0.9, float_type eps = 0.01,
-             float_type decay = 0.5, int_type epoch = 1, uint64_t ls = 20000000,
-             float_type lr_pow = 0.51, std::mt19937_64::result_type seed = 42) {
+  void train(float_type gamma = 0.9, float_type eps = 0.01, float_type decay = 0.5,
+             int_type epoch = 1, uint64_t ls = 20000000, float_type lr_pow = 0.51,
+             std::mt19937_64::result_type seed = 42) {
     reset_train();
 
     if constexpr (save_qs) {
@@ -178,36 +183,22 @@ class Env {
 
       for (int_type j = 0; j < ls; ++j) {
         int_type a{};
-        array_bq_type as = states_ != 0;
-        if (Fastor::any_of(as)) {
-          std::vector<int_type> idx_as;
-          for (int_type k = 0; k < n_queue; ++k) {
-            if (as(k)) {
-              idx_as.push_back(k);
-            }
-          }
-
-          if (eps_dis(rng) < eps) {
-            a = idx_as[std::uniform_int_distribution<int_type>(
-                0, idx_as.size() - 1)(rng)];
-          } else [[likely]] {
-            auto max_it = std::max_element(
-                iota_nq.begin(), iota_nq.end(),
-                [this](int_type largest, int_type current) {
-                  return std::apply(this->q_,
-                                    to_index(this->states_, idx_nq, largest)) <
-                         std::apply(this->q_,
-                                    to_index(this->states_, idx_nq, current));
-                });
-            a = *max_it;
-          }
+        if (eps_dis_(rng_) < eps) {
+          array_iq_type as = states_ != 0;
+          a = std::discrete_distribution<int_type>(as.data(), as.data() + n_queue)(rng_);
+        } else {
+          auto max_it = std::max_element(
+              iota_nq.begin(), iota_nq.end(), [this](int_type largest, int_type current) {
+                return std::apply(this->q_, to_index(this->states_, idx_nq, largest)) <
+                       std::apply(this->q_, to_index(this->states_, idx_nq, current));
+              });
+          a = *max_it;
         }
 
         auto nstates = step(a);
 
         auto max_nit = std::max_element(
-            iota_nq.begin(), iota_nq.end(),
-            [this, &nstates](int_type largest, int_type current) {
+            iota_nq.begin(), iota_nq.end(), [this, &nstates](int_type largest, int_type current) {
               return std::apply(this->q_, to_index(nstates, idx_nq, largest)) <
                      std::apply(this->q_, to_index(nstates, idx_nq, current));
             });
@@ -217,14 +208,11 @@ class Env {
 
         const auto r = reward(nstates, this->env_states_);
 
-        const float_type lr =
-            std::pow(std::apply(n_visit_, st_index) + 1, -lr_pow);
-        std::apply(q_, st_index) +=
-            lr * (r + gamma * nq - std::apply(q_, st_index));
+        const float_type lr = std::pow(std::apply(n_visit_, st_index) + 1, -lr_pow);
+        std::apply(q_, st_index) += lr * (r + gamma * nq - std::apply(q_, st_index));
 
         if constexpr (save_qs) {
-          std::copy(q_.data(), q_.data() + n_total,
-                    qs_.begin() + i * ls + j * n_total);
+          std::copy(q_.data(), q_.data() + n_total, qs_.begin() + i * ls + j * n_total);
         }
 
         std::apply(n_visit_, st_index) += 1;
@@ -233,8 +221,7 @@ class Env {
     }
   }
 
-  void from_array(const float_type* q, const int_type* n_visit, float_type* qs,
-                  size_t qs_size) {
+  void from_array(const float_type* q, const int_type* n_visit, float_type* qs, size_t qs_size) {
     q_ = q;
     n_visit_ = n_visit;
     if constexpr (save_qs) {
@@ -259,38 +246,18 @@ class Env {
   n_visit_type n_visit_;
   qs_type qs_;
 
-  static constexpr auto actions_ =
-      iq_type(const_cast<int_type*>(iota_nq.data()));
-  static constexpr array_iq_type max_lens_ = {max_lens...};
-  static constexpr auto transitions_ =
-      gen_trans<n_transition, n_queue, n_env>(idx_ne);
+  static constexpr auto actions_ = iq_type(const_cast<int_type*>(iota_nq.data()));
+  static constexpr array_iq_type max_lens_ = {max_lens_t...};
+  static constexpr auto transitions_ = gen_trans<n_transition, n_queue>(idx_ne);
 
-  std::mt19937_64 rng;
-  std::uniform_real_distribution<float_type> eps_dis;
-
-  template <int_type current, int_type... before, int_type... after>
-  static constexpr auto gen_inf_index(
-      std::integer_sequence<int_type, before...>,
-      std::integer_sequence<int_type, after...>) {
-    return std::make_tuple(Fastor::fseq<0, dim_queue[before]>{}...,
-                           Fastor::fseq<0, 1>{},
-                           Fastor::fseq<0, dim_queue[after + current + 1]>{}...,
-                           Fastor::fix<current>);
-  }
-
-  template <int_type... i>
-  static constexpr auto gen_inf_indices(
-      std::integer_sequence<int_type, i...> is) {
-    return std::make_tuple(gen_inf_index<i>(
-        std::make_integer_sequence<int_type, i>{},
-        std::make_integer_sequence<int_type, is.size() - i - 1>{})...);
-  }
+  std::mt19937_64 rng_;
+  std::uniform_real_distribution<float_type> eps_dis_;
 };
 
-template <int_type n_env_t, typename F, bool save_qs_t, int_type... max_lens>
-class LinearEnv : public Env<n_env_t, 2, F, save_qs_t, max_lens...> {
+template <int_type n_env_t, typename F, bool save_qs_t, int_type... max_lens_t>
+class LinearEnv : public Env<n_env_t, 2, F, save_qs_t, max_lens_t...> {
  public:
-  using parent_type = Env<n_env_t, 2, F, save_qs_t, max_lens...>;
+  using parent_type = Env<n_env_t, 2, F, save_qs_t, max_lens_t...>;
 
  protected:
   using parent_type::env_param_;
@@ -301,17 +268,16 @@ class LinearEnv : public Env<n_env_t, 2, F, save_qs_t, max_lens...> {
   using float_type = typename parent_type::float_type;
   using array_iq_type = typename parent_type::array_iq_type;
 
-  float_type reward(const array_iq_type& states,
-                    const array_iq_type& env_states) const override {
+  float_type reward(const array_iq_type& states, const array_iq_type& env_states) const override {
     return -(states(0) * env_param_(env_states(0), 0, 0) +
              states(1) * env_param_(env_states(1), 1, 0));
   }
 };
 
-template <int_type n_env_t, typename F, bool save_qs_t, int_type... max_lens>
-class ConvexEnv : public Env<n_env_t, 2, F, save_qs_t, max_lens...> {
+template <int_type n_env_t, typename F, bool save_qs_t, int_type... max_lens_t>
+class ConvexEnv : public Env<n_env_t, 2, F, save_qs_t, max_lens_t...> {
  public:
-  using parent_type = Env<n_env_t, 2, F, save_qs_t, max_lens...>;
+  using parent_type = Env<n_env_t, 2, F, save_qs_t, max_lens_t...>;
 
  protected:
   using parent_type::env_param_;
@@ -322,8 +288,7 @@ class ConvexEnv : public Env<n_env_t, 2, F, save_qs_t, max_lens...> {
   using float_type = typename parent_type::float_type;
   using array_iq_type = typename parent_type::array_iq_type;
 
-  float_type reward(const array_iq_type& states,
-                    const array_iq_type& env_states) const override {
+  float_type reward(const array_iq_type& states, const array_iq_type& env_states) const override {
     return -(states(0) * env_param_(env_states(0), 0, 0) +
              states(1) * states(1) * env_param_(env_states(1), 1, 0));
   }
